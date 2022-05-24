00a1c3a74d1c0fcfc0e49f57b90ed97b2bb220d3
5axes <5axes@users.noreply.github.com>
2022-05-24 11:12:15 +0200

    Update ProfilAnalyser.py

--- ProfilAnalyser.py
+++ ProfilAnalyser.py
@@ -8,13 +8,15 @@
 # 5@xes  limit analyse to user Profil debuging
 #
 # 29/11/2020 Modifications order
+#
 # V 1.1.0  14/12/2020 Add filter on Profils and Function show only difference thanks to csakip (https://github.com/csakip)
 # V 1.1.1  14/12/2020 Add Function Unselect ALL
 # V 1.1.2  15/12/2020 Add filter on valued parameters
 # V 1.1.3  20/09/2021 replace ' by " for filtering option
 #
 # V 1.2.0  01/05/2022 Update for Cura 5.0
-# V 1.2.1  10/05/2022 Add Version
+# V 1.2.1  10/05/2022 Add Version in the document name
+#
 #-----------------------------------------------------------------------------------------------------------------------------
 
 VERSION_QT5 = False
